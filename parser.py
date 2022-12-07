a92c1e875367f9da679b0cf20830c88a25959ded
IYunet <lbvfzx@gmail.com>
2022-12-07 12:23:02 +0300

    описание в парсеры

---
+++ parser.py
@@ -0,0 +1,116 @@
+import time
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+
+def get_clear_str(string): # функция, которая чистит строку, оставляя только буквы
+    return " ".join(re.findall("\w+", string))
+
+
+"""
+
+Парсер сайта https://tgstat.ru/ с помощью библиотеки BeautifulSoup
+
+На выходе получаем файл users.svg со статистикой по топовым телеграм каналам в 47 категориях.
+
+"""
+
+
+my_headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OSX 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",  "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"}
+session = requests.Session()
+base_url = "https://tgstat.ru"
+url = base_url + "/ratings/channels/blogs/"
+r = session.get(url, headers=my_headers, )
+soup = BeautifulSoup(r.text, 'html.parser')
+ratings_col = soup.find(id="sticky-left-column__inner")
+links = ratings_col.findAll("a")
+ratings = [link.get("href") for link in links if link.get("href") and link.get("href").startswith("/ratings")][1:] # получаем ссылки на страницы с топами по 47 категориям
+data = []
+t_rating = 1
+for rating in ratings: # заходим на страницу каждой категории, откуда будем брать каналы
+    try:
+        url = base_url + rating
+        url_t = url.split("?")
+        topic = url_t[0].split("/")[-1]
+        url_t[0] += "?sort=reach"
+        url = url_t[0]
+        r = session.get(url, headers=my_headers, )
+        soup = BeautifulSoup(r.text, 'html.parser')
+        channels_list = soup.find(id="sticky-center-column")
+        users_links = [a.get("href") for a in channels_list.findAll("a")] # ссылки на каналы в топе
+        t_user = 1
+        for user_link in users_links: # заходим на страницу канала, чтобы взять статистику
+            try:
+
+                row = {}
+
+                url = user_link
+                r = session.get(url, headers=my_headers, )
+                soup = BeautifulSoup(r.text, 'html.parser')
+
+                """
+
+                Анализируя html элементы парсим с них определнную информацию
+
+                """
+
+                name = soup.find("h1", class_="text-dark text-center text-sm-left").get_text()
+                name = get_clear_str(name)
+
+
+                profile_link = soup.find("a", class_="btn btn-outline-info btn-rounded px-3 py-05 font-14 text-truncate")
+                tg_link = profile_link.get("href")
+                username = get_clear_str(profile_link.get_text())
+
+                row["name"] = name
+                row["username"] = username
+                row["tg_link"] = tg_link
+                row["category"] = topic
+
+                stats = soup.find(id="sticky-center-column").findAll("div", class_="col-lg-6 col-md-12 col-sm-12")
+
+                """
+
+                Приватные каналы и публичные имеют разную статистику, парсим по-разному
+
+                """
+
+
+                if username=="приватный канал":
+                    valid_card = [0,2,3,4,5,6]
+                    for e,stat in enumerate(stats):
+                        if e in valid_card:
+                            value = get_clear_str(stat.find("h2").get_text())
+                            title = get_clear_str(
+                                stat.find("div", class_="position-absolute text-uppercase text-dark font-12").get_text())
+                            if title == "возраст канала":
+                                title = "дата создания"
+                                value = stat.find("b", class_="text-dark mr-2 font-20").get_text()
+                            row[title] = value
+                else:
+                    for stat in stats:
+                        value = get_clear_str(stat.find("h2").get_text())
+                        title = get_clear_str(stat.find("div", class_="position-absolute text-uppercase text-dark font-12").get_text())
+                        if title == "возраст канала":
+                            title = "дата создания"
+                            value = stat.find("b", class_="text-dark mr-2 font-20").get_text()
+                        row[title] = value
+
+                data.append(row)
+                pd.DataFrame(data).to_csv("users.csv") #сохраняем результат
+            except Exception as e:
+                print(str(e))
+                print("skip")
+                continue
+            finally:
+                print(f"{t_user} юзеров из {len(users_links)} || {t_rating} категорий из {len(ratings)}")
+                t_user += 1
+    except Exception as e:
+        print(e.message)
+    finally:
+        t_rating += 1
+        continue
+
+
--- tg_parser.py
+++ tg_parser.py
@@ -4,8 +4,37 @@ import time
 import asyncio
 from pyrogram.errors import FloodWait
 
-api_id = ""  # FROM TELEGRAM API
-api_hash = ""  # FROM TELEGRAM API
+
+"""
+
+Чтобы взять сообщения с телеграм канала, используется Pyrogram.
+Для его использования необходимо зарегистрировать приложение на https://my.telegram.org/apps.
+Отсюда нам нужны API_ID и API_HASH.
+Теперь c помощью Pyrogram мы можем выполнять любые действия от лица пользователя, с которого
+зарегистрировали приложение.
+
+Например:
+ - вступать в каналы (также в приватные)
+ - получать полную историю канала (что нам и нужно)
+ - писать сообщения
+
+
+Чтобы избежать возможной блокировки за флуд, я не вступал в закрытые каналы (можно делать это выборчно),
+а также добавил исключение в случае ошибки из-за флуда. 
+
+Хоть и тг говорит, сколько требуется подождать до следующего запроса, но факт, что после ожидания запрос пройдет.
+В случае, когда тг беспрерывно велел ждать, я подключал другое приложение со второго аккаунта.
+
+На выходе мы получаем файл data.csv, где сохраняются максимум последние 100 сообщений канала 
+(можно было сохранять и всю историю, запрос к api в любом случае 1 отправляется).
+
+
+Методы в Pyrogram тривиальны, поэтому код без комментариев.
+
+"""
+
+api_id = ""  # FROM https://my.telegram.org/apps
+api_hash = ""  # FROM https://my.telegram.org/apps
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
@@ -21,7 +50,7 @@ async def main():
         flood = []
         for channel in channels:
             try:
-                local_texts = []
+                local_texts = [] # сообщения с одного канала
                 async for message in app.get_chat_history(channel):
                     if (message.text):
                         info = {}
@@ -35,7 +64,7 @@ async def main():
                             info["date"] = int(time.mktime(message.date.timetuple()))
                         texts.append(info)
                         local_texts.append(info)
-                        pd.DataFrame(texts).to_csv("docs-buz-topor.csv")
+                        pd.DataFrame(texts).to_csv("data.csv")
                     if len(local_texts) > 100:
                         break
             except FloodWait as e:
